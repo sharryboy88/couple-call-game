992945ad82d4e3585922808c9ff5019e772387a3
sharryboy88 <sharryboy88@gmail.com>
2025-08-17 23:55:23 +0200

    chore: re-track docs folder (remove from .gitignore)

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/couple-call-game/', // exakt dein Repo-Name + führender/abschließender Slash
+  base: '/couple-call-game/'
 })
